aec9442b2a7f5d6ff4e6db3ce7150f04e45e7f12
student <student@K42-06>
2018-01-09 20:59:36 +0200

    a

---
+++ AspNet-Identity-2-Extending-Users-And-Roles-master/AspNetIdentity2ExtendingUsersAndRoles/Controllers/MyControllers/NavController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetIdentity2ExtendingUsersAndRoles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetIdentity2ExtendingUsersAndRoles.Controllers.MyControllers
+{
+    public class NavController : Controller
+    {
+        static List<Good> goods = GoodstRepository.GetRepository();
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> category = goods
+                .Select(x => x.GoodCategory)
+                .Distinct()
+                .ToList();
+            return PartialView(category);
+        }
+    }
+}
