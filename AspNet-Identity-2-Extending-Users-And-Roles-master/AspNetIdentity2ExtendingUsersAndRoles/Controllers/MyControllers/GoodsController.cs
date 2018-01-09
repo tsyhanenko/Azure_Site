b2952997348a7a2e00d63bf97d9858aec2741ab2
student <student@K42-06>
2018-01-09 20:10:39 +0200

    pagelonks

---
+++ AspNet-Identity-2-Extending-Users-And-Roles-master/AspNetIdentity2ExtendingUsersAndRoles/Controllers/MyControllers/GoodsController.cs
@@ -0,0 +1,71 @@
+﻿using AspNetIdentity2ExtendingUsersAndRoles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetIdentity2ExtendingUsersAndRoles.Controllers.MyControllers
+{
+    public class GoodsController : Controller
+    {
+        int pagesize = 3;
+        static List<Good> goods = GoodstRepository.GetRepository();
+        public ActionResult Index(int id = 1)
+        {
+            GoodsViewModel model = new GoodsViewModel
+            {
+                Goods = goods.Skip((id - 1) * pagesize).Take(pagesize),
+                Pages = new PageLinksModel
+                {
+                    CurrentPage = id,
+                    TotalItems = goods.Count(),
+                    CountPerPage = pagesize
+                }
+            };
+
+
+            return View(model);
+        }
+
+        public PartialViewResult GetGoodsList(int id = 1)
+        {
+            GoodsViewModel model = new GoodsViewModel
+            {
+                Goods = goods.Skip((id - 1) * pagesize).Take(pagesize),
+                Pages = new PageLinksModel
+                {
+                    CurrentPage = id,
+                    TotalItems = goods.Count(),
+                    CountPerPage = pagesize
+                }
+            };
+
+            return PartialView(model);
+        }
+
+
+        //TEST
+
+        public PartialViewResult GetCategoryList(string category)
+        {
+
+            GoodsViewModel model = new GoodsViewModel
+            {
+
+
+                Goods = goods.FindAll(x => x.GoodCategory == category),
+                Pages = new PageLinksModel
+                {
+                    CurrentPage = 1,
+                    TotalItems = goods.Count(),
+                    CountPerPage = pagesize
+                }
+            };
+
+            return PartialView(model);
+        }
+    }
+
+
+}
---
+++ AspNet-Identity-2-Extending-Users-And-Roles-master/AspNetIdentity2ExtendingUsersAndRoles/Helpers/CustomHelpersMethods.cs
@@ -0,0 +1,59 @@
+﻿using AspNetIdentity2ExtendingUsersAndRoles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace AspNetIdentity2ExtendingUsersAndRoles.Helpers
+{
+    public static class CustomHelpersMethods
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+        PageLinksModel pages, Func<int, string> func)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 1; i <= pages.CountPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", func(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == pages.CurrentPage)
+                    tag.AddCssClass("btn btn-danger");
+                else
+                    tag.AddCssClass("btn btn-outline-success");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+
+        public static MvcHtmlString PageLinks(this AjaxHelper html,
+           PageLinksModel pages, Func<int, string> func, string target)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 1; i <= pages.CountPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", func(i));
+                tag.InnerHtml = i.ToString();
+
+
+                AjaxOptions options = new AjaxOptions();
+                options.UpdateTargetId = target;
+                tag.MergeAttributes(options.ToUnobtrusiveHtmlAttributes());
+
+
+                tag.AddCssClass("btn btn-danger");
+
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
